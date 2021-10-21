2eac8da09250165e4e2d89ea332842d219c9af9f
Mariana Ayumi Tamay <maritamay4@gmail.com>
2021-10-21 07:32:08 -0300

    Aplicação de python e flask

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
 
-app = Flask("__name__")
+app = Flask("__name__", template_folder="scr/templates", static_folder="scr/static")
 
 @app.route("/")
-@app.route("/home")
+@app.route("/index")
 def home():
-    return render_template("pagina_inicial.html")
+    return render_template("paginainicial.html")
 
+@app.route("/sobremim")
+def sobremim():
+    return render_template("sobre_mim.html")
+
+@app.route("/curriculo")
+def curriculo():
+    return render_template("trabalhos.html")
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
