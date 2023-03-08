4b273fec78e451d2378373809578aab133afe434
Mariana Tamay <maritamay4@gmail.com>
2023-03-08 18:56:29 -0300

    Portfolio revivido

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\alexa\Desktop\myproject\venv
+set VIRTUAL_ENV=C:\trabalho_design_digital\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
