2fde508a4b4eff6ef02995f6b2b5abefc4b167fc
Mariana Tamay <maritamay4@gmail.com>
2023-03-18 15:34:57 -0300

    Corrigido app.py

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from jinja2 import Template
 
-app = Flask(__name__, template_folder="src/api/templates", static_folder="src/api/static")
+app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
